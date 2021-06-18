e8d3f1d574d3bb4af60145e521aa794bc55b4804
Riccardo Schirone <sirmy15@gmail.com>
2021-06-18 10:39:00 +0200

    core/cmd_descs: add support for default_mode
    Some commands might have one of the output modes as default one. If
    that's the case, RzCmd should be aware of that so that it can
    initializes everything needed for that default mode.
    
    For example, if command `c` supports TABLE, JSON and QUIET and you want
    to have `c`(with no suffix) print the TABLE mode, the parser would have
    to initialize the RzTable data structures anyway, so it must be aware of
    it.

--- librz/core/cmd_descs/cmd_descs.c
+++ librz/core/cmd_descs/cmd_descs.c
@@ -5503,6 +5503,7 @@ RZ_IPI void rzshell_cmddescs_init(RzCore *core) {
 	rz_warn_if_fail(i_cd);
 	RzCmdDesc *cmd_info_archs_cd = rz_cmd_desc_argv_state_new(core->rcmd, i_cd, "iA", RZ_OUTPUT_MODE_TABLE | RZ_OUTPUT_MODE_JSON | RZ_OUTPUT_MODE_QUIET, rz_cmd_info_archs_handler, &cmd_info_archs_help);
 	rz_warn_if_fail(cmd_info_archs_cd);
+	rz_cmd_desc_set_default_mode(cmd_info_archs_cd, RZ_OUTPUT_MODE_TABLE);
 
 	RzCmdDesc *cmd_kuery_cd = rz_cmd_desc_oldinput_new(core->rcmd, root_cd, "k", rz_cmd_kuery, &cmd_kuery_help);
 	rz_warn_if_fail(cmd_kuery_cd);
